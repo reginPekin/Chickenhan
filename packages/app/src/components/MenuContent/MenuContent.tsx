cf763deae7e223ede9be803bad4260cdafac524e
reginPekin <reginbegin@gmail.com>
2020-06-09 21:16:25 +0300

    Add ChatLine

--- packages/app/src/components/MenuContent/MenuContent.tsx
+++ packages/app/src/components/MenuContent/MenuContent.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
 
-import styles from './MenuContent.module.css';
+import { chats } from './consts';
 
-export const MenuContent: React.FC = () => {
-  return <div className={styles.menuContent}>MenuContent</div>;
+import { ChatLine } from '@chickenhan/components/src/ChatLine';
+import { Header } from '@chickenhan/components/src/Header';
+
+import styles from './MenuContent.module.css';
+import { MenuState } from '../MenuSidebar/consts';
+
+interface MenContentProps {
+  choosenTab: MenuState;
+}
+
+export const MenuContent: React.FC<MenContentProps> = ({ choosenTab }) => {
+  // бизнес логика и интерфейс, что, где и как?
+  function setLabel(): string {
+    switch (choosenTab) {
+      case 'discover':
+        return 'Discover';
+      case 'chats':
+        return 'Chats';
+      case 'profile':
+        return 'Profile';
+      case 'newChat':
+        return 'Add new chat';
+      default:
+        return 'Chats';
+    }
+  }
+
+  return (
+    <section className={styles.mainSection}>
+      <Header label={setLabel()} />
+      <section className={styles.scrolledChats}>
+        <div className={styles.menuContent}>
+          {chats.map((chat, key) => (
+            <ChatLine key={key} chat={chat} />
+          ))}
+        </div>
+      </section>
+    </section>
+  );
 };
