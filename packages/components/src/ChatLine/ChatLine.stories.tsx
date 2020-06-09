23fc187fd2c3f84891ddf546bc56e82e5882194e
reginPekin <reginbegin@gmail.com>
2020-06-09 21:13:05 +0300

    Add ChatLine component

--- packages/components/src/ChatLine/ChatLine.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-
-import { ChatLine } from './ChatLine';
-import { MOCK_CHAT_1 } from '../../__mocks__';
-
-storiesOf('Discover', module).add('discover card', () => {
-  return <ChatLine chat={MOCK_CHAT_1} />;
-});
--- packages/components/src/ChatLine/ChatLine.tsx
+++ packages/components/src/ChatLine/ChatLine.tsx
@@ -1,18 +1,52 @@
-import React from 'react';
-import { Chat } from '../../types';
+import React, { createContext } from 'react';
+// import { Chat } from '../../types';
+import cx from 'classnames';
+
+import { Chat } from '../../../app/src/components/MenuContent/consts';
+import { Avatar } from '../Avatar';
+
+import styles from './ChatLine.module.css';
 
 interface ChatLineProps {
   chat: Chat;
 }
 
 export const ChatLine: React.FC<ChatLineProps> = ({ chat }) => {
-  return (
-    <div>
-      <img src={chat.avatar} width={32} />
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <span>{chat.name}</span>
-        <span>{chat.userCount}</span>
+  function renderUserCount(): React.ReactNode {
+    if (!chat.userCount) return null;
+    return <span className={styles.userCount}>{chat.userCount} участник</span>; // язык и падежи
+  }
+
+  function renderMessageSection(): React.ReactNode {
+    return (
+      <div className={styles.messageSection}>
+        <span
+          className={cx(
+            styles.lastMessage,
+            chat.type === 'dialog'
+              ? styles.dialogLastMessage
+              : styles.chatLastMessage,
+          )}
+        >
+          {chat.lastMessage}
+        </span>
+        {renderUserCount()}
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <section className={styles.chatLine}>
+      <div className={styles.avatar}>
+        <Avatar url={chat.avatar} chatType={chat.type} />
+      </div>
+      <div className={styles.chatInfo}>
+        <div className={styles.headInfo}>
+          <span className={styles.name}>{chat.name}</span>
+          <span className={styles.lastDateMessage}>{chat.lastDateMessage}</span>
+        </div>
+        {renderMessageSection()}
+      </div>
+    </section>
   );
 };
