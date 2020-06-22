a059188edd3bb89675bec802535e1ba30fafb672
reginPekin <reginbegin@gmail.com>
2020-06-22 11:39:22 +0300

    Add Chat and Message interfaces

--- packages/components/src/types.ts
+++ packages/components/src/types.ts
@@ -5,8 +5,42 @@ export interface User {
   avatar: string;
 }
 
+interface Opponent {
+  login: string;
+  isOnline: boolean; // на бэкэ online
+}
+
+export type ChatType = 'dialog' | 'public' | 'private';
 export interface Chat {
+  isDialog?: Opponent;
+  id: string;
+  lastMessage?: string;
+  lastDateMessage?: string;
   avatar: string;
-  name: string;
+  type: ChatType;
+  // parameters below exists just for public & private types
+  name?: string;
   userCount?: number;
+}
+
+interface Picture {
+  id: string;
+  url: string;
+  // height: number;
+  // width: number;
+}
+
+interface Author {
+  userId: string;
+  name: string;
+  avatar: string;
+  online: boolean;
+}
+
+export interface Message {
+  author: Author;
+  text: string;
+  date: number;
+  messageId: string;
+  pictures?: Array<Picture>;
 }
