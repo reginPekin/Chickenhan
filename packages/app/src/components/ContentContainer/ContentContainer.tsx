665ed7ecc63130d1ba93b4e2871abc5e390e9a8d
reginPekin <reginbegin@gmail.com>
2020-06-22 11:38:41 +0300

    Add elements to ContentContainer

--- packages/app/src/components/ContentContainer/ContentContainer.tsx
+++ packages/app/src/components/ContentContainer/ContentContainer.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import styles from './ContentContainer.module.css';
 
+import { ChatHeader } from '@chickenhan/components/src/ChatHeader';
+import { WriteBox } from '@chickenhan/components/src/WriteBox';
+import { Message } from '@chickenhan/components/src/Message';
+
+import {
+  MOCK_MESSAGE_LINE_1,
+  MOCK_CHAT_1,
+} from '@chickenhan/components/src/__mocks__';
+
 export const ContentContainer: React.FC = () => {
-  return <div className={styles.contentContainer}>Hi</div>;
+  return (
+    <main className={styles.contentContainer}>
+      <ChatHeader chat={MOCK_CHAT_1} />
+      <section className={styles.contentSection}>
+        <Message message={MOCK_MESSAGE_LINE_1} />
+      </section>
+      <WriteBox />
+    </main>
+  );
 };
