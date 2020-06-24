b617da4927f863ee6cf46713d2af55bb322562ec
Regina  Valetova <reginavaletova@MacBook-Pro-Regina.local>
2020-06-24 19:01:00 +0300

    Add props to icons

--- packages/components/src/Icons/index.tsx
+++ packages/components/src/Icons/index.tsx
@@ -7,10 +7,16 @@ export interface IconProps {
   stroke?: string | undefined;
   width?: number;
   height?: number;
+  style?: React.CSSProperties;
+  className?: string;
+  onClick?: () => void;
 }
 
-export const AddChatIcon: React.FC<IconProps> = ({ fill = 'none' }) => (
-  <svg width={24} height={24} viewBox="0 0 93 93">
+export const AddChatIcon: React.FC<IconProps> = ({
+  fill = 'none',
+  style = {},
+}) => (
+  <svg width={24} height={24} viewBox="0 0 93 93" style={style}>
     <rect
       x={1.5}
       y={1.5}
@@ -26,17 +32,13 @@ export const AddChatIcon: React.FC<IconProps> = ({ fill = 'none' }) => (
   </svg>
 );
 
-export const SearchIcon: React.FC<IconProps> = ({
-  stroke = '#C4C4C4',
-  width = 24,
-  height = 24,
-}) => (
-  <svg width={width} height={height} viewBox="0 0 48 50">
+export const SearchIcon: React.FC<IconProps> = props => (
+  <svg viewBox="0 0 48 50" {...props}>
     <circle
       cx={16.5}
       cy={16.5}
       r={14.5}
-      stroke={stroke}
+      stroke={props.stroke || '#C4C4C4'}
       strokeWidth={4}
       fill="none"
     />
@@ -47,25 +49,21 @@ export const SearchIcon: React.FC<IconProps> = ({
       height={27.1723}
       rx={2.327}
       transform="rotate(-41.3881 25.6964 28.9453)"
-      fill={stroke}
+      fill={props.stroke || '#C4C4C4'}
     />
   </svg>
 );
 
-export const DeleteIcon: React.FC<IconProps> = ({
-  width = 18,
-  height = 18,
-  fill = '#C4C4C4',
-}) => (
-  <svg width={width} height={height} viewBox="0 0 64 64">
+export const DeleteIcon: React.FC<IconProps> = props => (
+  <svg viewBox="0 0 64 64" {...props}>
     <path
       d="M63.381 63.381C62.5557 64.2063 61.2177 64.2063 60.3924 63.381L0.61898 3.60764C-0.206319 2.78235 -0.20632 1.44427 0.618978 0.618974C1.44428 -0.206325 2.78235 -0.206324 3.60765 0.618974L63.381 60.3923C64.2063 61.2176 64.2063 62.5557 63.381 63.381Z"
-      fill={fill}
+      fill={props.fill || '#C4C4C4'}
       strokeWidth={4}
     />
     <path
       d="M0.618974 63.381C-0.206325 62.5557 -0.206325 61.2177 0.618974 60.3924L60.3924 0.618985C61.2177 -0.206313 62.5557 -0.206315 63.381 0.618983C64.2063 1.44428 64.2063 2.78235 63.381 3.60765L3.60764 63.381C2.78235 64.2063 1.44427 64.2063 0.618974 63.381Z"
-      fill={fill}
+      fill={props.fill || '#C4C4C4'}
       strokeWidth={4}
     />
   </svg>
