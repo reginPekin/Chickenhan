ba6aac5b3ff20c4a682974d09bbf417cdb166f97
reginPekin <reginbegin@gmail.com>
2020-07-09 18:25:38 +0300

    Add avatar loader icon

--- packages/app/src/components/Icons.tsx
+++ packages/app/src/components/Icons.tsx
@@ -130,6 +130,28 @@ export const DeleteIcon: React.FC<IconProps> = props => (
   </svg>
 );
 
+export const AvatarLoaderIcon: React.FC<IconProps> = props => (
+  <svg
+    width={props.width || 100}
+    height={props.height || 100}
+    viewBox="0 0 94 94"
+    fill="none"
+  >
+    <circle cx={47} cy={47} r={45.5} stroke="#FFC224" strokeWidth={3} />
+    <circle cx={46.5} cy={43.5} r={15} stroke="#FFC224" strokeWidth={3} />
+    <path
+      d="M79 80.5L74.9817 71.518C72.0877 65.049 66.997 59.8134 60.6117 56.739L57 55"
+      stroke="#FFC224"
+      strokeWidth={3}
+    />
+    <path
+      d="M14.5 79L17.9944 71.356C20.9061 64.9867 25.9543 59.8368 32.2642 56.7987L36 55"
+      stroke="#FFC224"
+      strokeWidth={3}
+    />
+  </svg>
+);
+
 export const GoogleIcon: React.FC<IconProps> = props => (
   <svg width={25} height={25} {...props}>
     <g fill="none">
