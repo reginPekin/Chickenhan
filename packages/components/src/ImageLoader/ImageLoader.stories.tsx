01db08bd4e8b572334af214a8cb838fc61290063
reginPekin <reginbegin@gmail.com>
2020-07-09 18:29:24 +0300

    Fix image loader

--- packages/components/src/ImageLoader/ImageLoader.stories.tsx
+++ packages/components/src/ImageLoader/ImageLoader.stories.tsx
@@ -4,6 +4,32 @@ import { ImageLoader } from './ImageLoader';
 
 import { storiesOf } from '@storybook/react';
 
+interface IconProps {
+  width: number;
+  height: number;
+}
+
+const AvatarLoaderIcon: React.FC<IconProps> = props => (
+  <svg viewBox="0 0 94 94" fill="none" {...props}>
+    <circle cx="47" cy="47" r="45.5" stroke="#FFC224" strokeWidth="3" />
+    <circle cx="46.5" cy="43.5" r="15" stroke="#FFC224" strokeWidth="3" />
+    <path
+      d="M79 80.5L74.9817 71.518C72.0877 65.049 66.997 59.8134 60.6117 56.739L57 55"
+      stroke="#FFC224"
+      strokeWidth="3"
+    />
+    <path
+      d="M14.5 79L17.9944 71.356C20.9061 64.9867 25.9543 59.8368 32.2642 56.7987L36 55"
+      stroke="#FFC224"
+      strokeWidth="3"
+    />
+  </svg>
+);
+
 storiesOf('ImageLoader', module).add('Simple image loader', () => (
-  <ImageLoader />
+  <section style={{ margin: '20px' }}>
+    <ImageLoader loadedImgStyle={{ height: '150px', maxHeight: '150px' }}>
+      <AvatarLoaderIcon width={92} height={92} />
+    </ImageLoader>
+  </section>
 ));
--- packages/components/src/ImageLoader/ImageLoader.tsx
+++ packages/components/src/ImageLoader/ImageLoader.tsx
@@ -2,38 +2,51 @@ import React, { useRef, useState, useEffect } from 'react';
 
 import styles from './ImageLoader.module.css';
 
-export const ImageLoader: React.FC = () => {
-  const [previewUrl, setPreviewUrl] = useState<string | ArrayBuffer | null>();
+import { handleFile } from '../utils';
+
+interface ImageLoaderProps {
+  files?: File[];
+  setFiles?: (file: File[]) => void;
+
+  children?: JSX.Element;
+  loadedImgStyle?: React.CSSProperties;
+}
+
+export const ImageLoader: React.FC<ImageLoaderProps> = ({
+  files = [],
+  setFiles = (): void => undefined,
+
+  children,
+  loadedImgStyle = {},
+}) => {
+  const [previewUrl, setPreviewUrl] = useState<string | ArrayBuffer | null>(
+    null,
+  );
 
   const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const handleFile = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    if (!event || !event.target || !event.target.files) {
+  useEffect(() => {
+    if (files.length > 0) {
+      handleFile(files, file => setPreviewUrl(file));
       return;
     }
+    setPreviewUrl(null);
+  }, [files]);
 
-    const files: File[] = Array.prototype.slice.call(event.target.files);
+  // не получается здесь тип задать
+  function renderLoadedImage(): any {
+    if (!previewUrl) return children;
 
-    for (const file of files) {
-      const reader = new FileReader();
-
-      reader.onload = async (): Promise<void> => {
-        const b64image = reader.result;
-
-        const img = new Image();
-        img.src = `${b64image}`;
-
-        const form = new FormData();
-        form.append('photo', file);
-
-        setPreviewUrl(reader.result);
-      };
-
-      reader.onabort = () => console.log('file reading was aborted');
-      reader.onerror = () => console.log('file reading has failed');
-      reader.readAsDataURL(file);
-    }
-  };
+    return (
+      <div style={loadedImgStyle}>
+        <img
+          height="100%"
+          src={previewUrl as string}
+          className={styles.loadedImg}
+        />
+      </div>
+    );
+  }
 
   return (
     <section>
@@ -42,16 +55,20 @@ export const ImageLoader: React.FC = () => {
         ref={inputRef}
         type="file"
         accept="image/png, image/jpeg"
-        onChange={handleFile}
+        onChange={(event): void => {
+          if (event && event.target && event.target.files) {
+            setFiles(Array.prototype.slice.call(event.target.files));
+            handleFile(event.target.files, file => setPreviewUrl(file));
+          }
+        }}
         className={styles.input}
       />
-      <img src={previewUrl as string} />
-      <span
+      <div
         onClick={(): void => inputRef.current?.click()}
         className={styles.clicker}
       >
-        I am clicker
-      </span>
+        {renderLoadedImage()}
+      </div>
     </section>
   );
 };
