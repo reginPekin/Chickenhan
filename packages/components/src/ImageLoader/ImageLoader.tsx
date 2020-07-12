34af65044a1dbfca09e87319a10c1b2d904282f7
reginPekin <reginbegin@gmail.com>
2020-07-13 01:57:03 +0300

    Rename function

--- packages/components/src/ImageLoader/ImageLoader.tsx
+++ packages/components/src/ImageLoader/ImageLoader.tsx
@@ -8,21 +8,21 @@ import { handleFile } from '../utils';
 
 interface ImageLoaderProps {
   files?: File[];
-  setFiles?: (file: File[]) => void;
+  onFileLoaded?: (file: File[]) => void;
 
   children?: JSX.Element;
-  loadedImgStyle?: React.CSSProperties;
+  previewImage?: string | ArrayBuffer | null;
 }
 
 export const ImageLoader: React.FC<ImageLoaderProps> = ({
   files = [],
-  setFiles = (): void => undefined,
+  onFileLoaded = (): void => undefined,
 
   children,
-  loadedImgStyle = {},
+  previewImage = null,
 }) => {
   const [previewUrl, setPreviewUrl] = useState<string | ArrayBuffer | null>(
-    null,
+    previewImage,
   );
 
   const inputRef = useRef<HTMLInputElement | null>(null);
@@ -32,7 +32,7 @@ export const ImageLoader: React.FC<ImageLoaderProps> = ({
       handleFile(files, file => setPreviewUrl(file));
       return;
     }
-    setPreviewUrl(null);
+    setPreviewUrl(previewImage || null);
   }, [files]);
 
   function renderLoadedImage(): React.ReactNode {
@@ -48,8 +48,8 @@ export const ImageLoader: React.FC<ImageLoaderProps> = ({
         type="file"
         accept="image/png, image/jpeg"
         onChange={(event): void => {
-          if (event && event.target && event.target.files) {
-            setFiles(Array.prototype.slice.call(event.target.files));
+          if (event?.target?.files) {
+            onFileLoaded(Array.prototype.slice.call(event.target.files));
             handleFile(event.target.files, file => setPreviewUrl(file));
           }
         }}
