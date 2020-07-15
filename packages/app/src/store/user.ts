eb410142d01ea65d8b30f71f8f30133a63150513
reginPekin <reginbegin@gmail.com>
2020-07-16 00:35:41 +0300

    Add user info to the store

--- packages/app/src/store/user.ts
+++ packages/app/src/store/user.ts
@@ -1,14 +1,20 @@
 import { createStore } from '../utils/createStore';
 
 interface User {
-  name: string;
+  id: string;
+  login: string;
+  isOnline: boolean;
+  avatar: string;
 }
 
 // too complex return type
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function createUserStore() {
   const initialState: User = {
-    name: 'baranka',
+    id: '23423423',
+    login: 'Begin',
+    isOnline: true,
+    avatar: `https://imgtest.mir24.tv/uploaded/images/crops/2018/September/870x489_0x47_detail_crop_af43017eebc9b37984240ffb3877c8aef3aa8f499eaf279991aa312a36b83f25.jpg`,
   };
 
   const [state, setState, useState, useSelector] = createStore(initialState);
