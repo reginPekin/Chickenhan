9788055a4cf0d5af091d1b3cd4920a6ffd1d76d7
reginPekin <reginbegin@gmail.com>
2020-07-16 00:47:23 +0300

    Change values through storage

--- packages/app/src/components/ProfilePopup/ProfilePopup.tsx
+++ packages/app/src/components/ProfilePopup/ProfilePopup.tsx
@@ -1,43 +1,77 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import styles from './ProfilePopup.module.css';
-
-import { MOCK_USER_1 } from '@chickenhan/components/src/__mocks__';
 
 import { ImageLoader } from '@chickenhan/components/src/ImageLoader';
 import { BasicInput } from '@chickenhan/components/src/BasicInput';
 
 import { handleFile } from '@chickenhan/components/src/utils';
 
+import { useStore } from '../../store';
+
 export const ProfilePopup: React.FC = () => {
-  const [avatar, setAvatar] = useState<string | ArrayBuffer | null>(
-    MOCK_USER_1.avatar,
-  ); // use store
+  const store = useStore();
+
+  const inpuRef = useRef<HTMLInputElement>(null);
+
+  const isProfileOpen = store.local.useSelector(local => local.isProfileOpen);
+
+  const [user] = store.user.useState();
+
+  const [newName, setNewName] = useState<string>('');
+  const [loginError, setLoginError] = useState<string>('');
 
   return (
-    <main className={styles.profilePopup}>
+    <main
+      className={styles.profilePopup}
+      style={{ display: isProfileOpen ? 'flex' : 'none' }}
+    >
       <header className={styles.profilePopupModal}>Edit profile</header>
       <section className={styles.sectionProfilePopup}>
         <div className={styles.modalSection}>
           <ImageLoader
-            previewImage={avatar}
+            previewImage={user.avatar}
             onFileLoaded={(files): void =>
-              handleFile(files, file => setAvatar(file))
+              handleFile(files, file => {
+                store.user.update({ avatar: file as string });
+              })
             }
           />
         </div>
         <div className={styles.modalSection}>
           <div className={styles.labelContainer}>
             <label className={styles.label}>Name</label>
+
+            {loginError && <span className={styles.error}>{loginError}</span>}
+
             <div className={styles.input}>
-              <BasicInput placeholder={MOCK_USER_1.login} />
+              <BasicInput
+                placeholder={user.login}
+                ref={inpuRef}
+                onChange={(event): void => {
+                  setNewName(event.target.value);
+                }}
+              />
             </div>
           </div>
         </div>
       </section>
       <footer className={styles.profilePopupModal}>
         <div className={styles.buttonWrapper}>
-          <button className={styles.button}>Save</button>
+          <button
+            className={styles.button}
+            onClick={(): void => {
+              // ещё проверять на уникальность нового логина
+              if (newName && newName !== user.login) {
+                store.user.update({ login: newName });
+                setLoginError('');
+              } else {
+                setLoginError('Empty space');
+              }
+            }}
+          >
+            Save
+          </button>
         </div>
       </footer>
     </main>
