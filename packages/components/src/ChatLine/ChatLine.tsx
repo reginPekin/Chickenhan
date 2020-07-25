1c011e89e828338427ddf556ad92c372fb1a264c
reginPekin <reginbegin@gmail.com>
2020-07-25 17:37:37 +0300

    Use hr instead border-bottom

--- packages/components/src/ChatLine/ChatLine.tsx
+++ packages/components/src/ChatLine/ChatLine.tsx
@@ -19,44 +19,68 @@ export const ChatLine: React.FC<ChatLineProps> = React.memo(({ chat }) => {
 
   if (chat.opponent)
     return (
+      <main>
+        <section className={styles.chatLine}>
+          <div className={styles.avatar}>
+            <Avatar url={chat.avatar} chatType={chat.type} />
+            {chat.opponent.isOnline && <div className={styles.onlineCircle} />}
+          </div>
+          <div className={styles.chatInfo}>
+            <div className={styles.headInfo}>
+              <span className={styles.name}>{chat.opponent.login}</span>
+              <span className={styles.lastDateMessage}>
+                {chat.lastDateMessage}
+              </span>
+            </div>
+            <div className={styles.messageSection}>
+              <span
+                className={cx(styles.lastMessage, styles.dialogLastMessage)}
+              >
+                {chat.lastMessage}
+              </span>
+            </div>
+          </div>
+        </section>
+        <hr
+          style={{
+            backgroundColor: 'var(--hover-grey)',
+            borderRadius: '50%',
+            height: 1,
+            margin: '0 12px',
+          }}
+        />
+      </main>
+    );
+
+  return (
+    <main>
       <section className={styles.chatLine}>
         <div className={styles.avatar}>
           <Avatar url={chat.avatar} chatType={chat.type} />
-          {chat.opponent.isOnline && <div className={styles.onlineCircle} />}
         </div>
         <div className={styles.chatInfo}>
           <div className={styles.headInfo}>
-            <span className={styles.name}>{chat.opponent.login}</span>
+            <span className={styles.name}>{chat.name}</span>
             <span className={styles.lastDateMessage}>
               {chat.lastDateMessage}
             </span>
           </div>
           <div className={styles.messageSection}>
-            <span className={cx(styles.lastMessage, styles.dialogLastMessage)}>
+            <span className={cx(styles.lastMessage, styles.chatLastMessage)}>
               {chat.lastMessage}
             </span>
+            {renderUserCount()}
           </div>
         </div>
       </section>
-    );
-
-  return (
-    <section className={styles.chatLine}>
-      <div className={styles.avatar}>
-        <Avatar url={chat.avatar} chatType={chat.type} />
-      </div>
-      <div className={styles.chatInfo}>
-        <div className={styles.headInfo}>
-          <span className={styles.name}>{chat.name}</span>
-          <span className={styles.lastDateMessage}>{chat.lastDateMessage}</span>
-        </div>
-        <div className={styles.messageSection}>
-          <span className={cx(styles.lastMessage, styles.chatLastMessage)}>
-            {chat.lastMessage}
-          </span>
-          {renderUserCount()}
-        </div>
-      </div>
-    </section>
+      <hr
+        style={{
+          backgroundColor: 'var(--hover-grey)',
+          borderRadius: '50%',
+          height: 1,
+          margin: '0 12px',
+        }}
+      />
+    </main>
   );
 });
