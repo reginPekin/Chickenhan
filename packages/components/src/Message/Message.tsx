e3815bebe5b6e32a12fa9e3182f27e2a085a5de4
reginPekin <reginbegin@gmail.com>
2020-08-13 23:03:28 +0300

    Change style of button and menuIcon

--- packages/components/src/Message/Message.tsx
+++ packages/components/src/Message/Message.tsx
@@ -12,45 +12,52 @@ import styles from './Message.module.css';
 
 interface MessageProps {
   message: TypeMessage;
+  pending?: boolean;
 }
 
-export const Message: React.FC<MessageProps> = React.memo(({ message }) => {
-  function renderAuthor(): JSX.Element {
-    return (
-      <div className={styles.nameAndDate}>
-        <span>{message.author.login}</span>
-        <span
-          style={{ display: message.author.isOnline ? 'block' : 'none' }}
-          className={styles.onlineCircle}
-        />
-        <time className={styles.date}>{parseTime(message.date)}</time>
-      </div>
-    );
-  }
-
-  function renderPictures(): JSX.Element {
-    return (
-      <article className={styles.picturesArray}>
-        {message.pictures?.map(picture => (
-          <div
-            className={cx(styles.pictureSection, styles.messageFile)}
-            key={picture.id}
-          >
-            <img className={styles.picture} src={picture.url} />
+export const Message: React.FC<MessageProps> = React.memo(
+  ({ message, pending }) => {
+    function renderAuthor(): JSX.Element {
+      return (
+        <div className={styles.nameAndDate}>
+          <span>{message.author.login}</span>
+          <div>
+            <span
+              style={{ display: message.author.isOnline ? 'block' : 'none' }}
+              className={styles.onlineCircle}
+            />
           </div>
-        ))}
+          <time className={styles.date}>{parseTime(message.date)}</time>
+          {/* статус отправки сообщения (если в режиме ожидания) */}
+          {/* <div>{pending ? 'Peeending' : ''}</div> */}
+        </div>
+      );
+    }
+
+    function renderPictures(): JSX.Element {
+      return (
+        <article className={styles.picturesArray}>
+          {message.pictures?.map(picture => (
+            <div
+              className={cx(styles.pictureSection, styles.messageFile)}
+              key={picture.id}
+            >
+              <img className={styles.picture} src={picture.url} />
+            </div>
+          ))}
+        </article>
+      );
+    }
+
+    return (
+      <article className={styles.messageArticle}>
+        <Avatar url={message.author.avatar} width={40} />
+        <div className={styles.textInfo}>
+          {renderAuthor()}
+          <span className={styles.messageText}>{message.text}</span>
+          {renderPictures()}
+        </div>
       </article>
     );
-  }
-
-  return (
-    <article className={styles.messageArticle}>
-      <Avatar url={message.author.avatar} width={40} />
-      <div className={styles.textInfo}>
-        {renderAuthor()}
-        <span className={styles.messageText}>{message.text}</span>
-        {renderPictures()}
-      </div>
-    </article>
-  );
-});
+  },
+);
