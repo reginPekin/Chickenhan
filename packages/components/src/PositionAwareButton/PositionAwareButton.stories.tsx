6a875760834efced58f8a603cbb168b855e2f5be
reginPekin <reginbegin@gmail.com>
2020-08-23 15:21:28 +0300

    Unify PositionAwareButton

--- packages/components/src/PositionAwareButton/PositionAwareButton.stories.tsx
+++ packages/components/src/PositionAwareButton/PositionAwareButton.stories.tsx
@@ -5,8 +5,20 @@ import { PositionAwareButton } from './PositionAwareButton';
 
 storiesOf('PositionAwareButton', module).add('Simple', () => {
   return (
-    <div style={{ margin: '100px', width: '100px', height: '40px' }}>
-      <PositionAwareButton>Hi</PositionAwareButton>
-    </div>
+    <section>
+      <div style={{ margin: '100px', width: '100px', height: '40px' }}>
+        <PositionAwareButton>Simple button</PositionAwareButton>
+      </div>
+      <div style={{ margin: '100px', width: '100px', height: '40px' }}>
+        <PositionAwareButton
+          backgroundColor="var(--black)"
+          hoveredColor="var(--hover-black)"
+          clickedColor="var(--click-black)"
+          textColor="var(--white)"
+        >
+          Black button
+        </PositionAwareButton>
+      </div>
+    </section>
   );
 });
--- packages/components/src/PositionAwareButton/PositionAwareButton.tsx
+++ packages/components/src/PositionAwareButton/PositionAwareButton.tsx
@@ -1,18 +1,32 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 import styles from './PositionAwareButton.module.css';
 
 interface BtnPosnAwrProps {
   children: string;
+
+  backgroundColor?: string;
+  hoveredColor?: string;
+  clickedColor?: string;
+  textColor?: string;
+
   onClick?: () => void;
 }
 
 export const PositionAwareButton: React.FC<BtnPosnAwrProps> = ({
   children,
+
+  backgroundColor = 'var(--main-yellow)',
+  hoveredColor = '#ffb100',
+  clickedColor = 'var(--hover-main-yellow)',
+  textColor = 'var(--light-black)',
+
   onClick = (): void => undefined,
 }) => {
   const btnPosnAwrRef = useRef<HTMLButtonElement | null>(null);
   const spanRef = useRef<HTMLSpanElement | null>(null);
+
+  const [isButtonHover, setIsButtonHover] = useState<boolean>(false);
 
   const listener = (event: MouseEvent): void => {
     if (btnPosnAwrRef.current && spanRef.current) {
@@ -42,13 +56,24 @@ export const PositionAwareButton: React.FC<BtnPosnAwrProps> = ({
 
   return (
     <button
-      style={{ position: 'relative' }}
+      onMouseEnter={(): void => setIsButtonHover(true)}
+      onMouseLeave={(): void => setIsButtonHover(false)}
+      style={{
+        position: 'relative',
+        backgroundColor: isButtonHover ? hoveredColor : backgroundColor,
+      }}
       ref={btnPosnAwrRef}
       className={styles.btnPosnawr}
       onClick={onClick}
     >
-      <span ref={spanRef} className={styles.span} />
-      <span className={styles.spanText}>{children}</span>
+      <span
+        ref={spanRef}
+        className={styles.span}
+        style={{ backgroundColor: clickedColor }}
+      />
+      <span className={styles.spanText} style={{ color: textColor }}>
+        {children}
+      </span>
     </button>
   );
 };
