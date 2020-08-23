19d8eb8404315cd9eb27784099548e6d7cbb6a99
reginPekin <reginbegin@gmail.com>
2020-08-23 15:45:35 +0300

    Create request handler

---
+++ packages/components/sdk/convert.ts
@@ -0,0 +1,57 @@
+function convertCase(incomingElement: any, convert: any): any {
+  const convertedObject: Record<string, any> = {};
+
+  const convertArray = (array: any[]) => {
+    const convertedArray = array.map(element => convertCase(element, convert));
+
+    return convertedArray;
+  };
+
+  const checkForObject = (element: any): boolean =>
+    typeof element === 'object' && !element.length;
+
+  const checkForArray = (element: any): boolean =>
+    typeof element === 'object' && element.length;
+
+  if (checkForObject(incomingElement)) {
+    for (const property in incomingElement) {
+      const snakeKey: string = convert(property);
+
+      if (checkForObject(incomingElement[property])) {
+        const convertedValues = convertCase(incomingElement[property], convert);
+
+        convertedObject[snakeKey] = convertedValues;
+        continue;
+      }
+
+      if (checkForArray(incomingElement[property])) {
+        const convertedArray = convertArray(incomingElement[property]);
+
+        convertedObject[snakeKey] = convertedArray;
+        continue;
+      }
+
+      convertedObject[snakeKey] = incomingElement[property];
+    }
+
+    return convertedObject;
+  }
+
+  if (checkForArray(incomingElement)) {
+    return convertArray(incomingElement);
+  }
+
+  return incomingElement;
+}
+
+export function camelToSnake(camel: Record<string, any>): Record<string, any> {
+  return convertCase(camel, (word: string): string => {
+    return word.replace(/([A-Z])/g, '_$1').toLowerCase();
+  });
+}
+
+export function snakeToCamel(snake: Record<string, any>): Record<string, any> {
+  return convertCase(snake, (word: string): string => {
+    return word.replace(/(_\w)/g, element => element[1].toUpperCase());
+  });
+}
---
+++ packages/components/sdk/utils.ts
@@ -0,0 +1,71 @@
+import { snakeToCamel, camelToSnake } from './convert';
+
+export interface Context {
+  token?: string;
+  apiUrl: string;
+}
+
+interface OptionsProps {
+  method?: 'POST' | 'GET' | 'PATCH' | 'DELETE';
+  headers?: { [param: string]: string };
+  body?: Object;
+  queryParams?: URLSearchParams;
+}
+
+interface RequestProps {
+  ctx: Context;
+  url: string;
+  options?: OptionsProps;
+}
+
+class ChickenhanError {
+  public code: string;
+  public name: string;
+  public desc: string;
+
+  constructor(code: string, name: string, desc: string) {
+    this.code = code;
+    this.name = name;
+    this.desc = desc;
+  }
+}
+
+const defaultOptions: OptionsProps = {
+  method: 'POST',
+  headers: {},
+};
+
+export async function request<T>({
+  ctx,
+  url,
+  options,
+}: RequestProps): Promise<T> {
+  const queryParams = options?.queryParams;
+
+  const searchParams = queryParams
+    ? new URLSearchParams(camelToSnake(queryParams))
+    : '';
+  const endpoint = ctx.apiUrl + url + searchParams;
+
+  const compiledOptions = { ...defaultOptions, ...options };
+
+  if (ctx.token && compiledOptions.headers) {
+    compiledOptions.headers['token'] = ctx.token;
+  }
+
+  if (compiledOptions.body) {
+    const convertedBodyToSnake = camelToSnake(compiledOptions.body);
+    compiledOptions.body = JSON.stringify(convertedBodyToSnake);
+  }
+
+  try {
+    const response = await fetch(endpoint, compiledOptions as any);
+    const body = response.json();
+
+    return snakeToCamel(body) as T;
+  } catch (errorJson) {
+    // приходит в виде JSON
+    const error = snakeToCamel(JSON.parse(errorJson));
+    throw new ChickenhanError(error.code, error.name, error.desc);
+  }
+}
