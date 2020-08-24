d569bc601967a97b75fd0ce8b9f1009dd8b10919
reginPekin <reginbegin@gmail.com>
2020-08-24 21:12:03 +0300

    Add options

--- packages/components/src/ChatHeader/ChatHeader.tsx
+++ packages/components/src/ChatHeader/ChatHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 
 import { Avatar } from '@chickenhan/components/src/Avatar';
 
@@ -10,44 +10,71 @@ import { MoreIcon } from '../Icons';
 
 interface ChatHeader {
   chat: Chat;
+  leaveChat?: () => void;
 }
 
-export const ChatHeader: React.FC<ChatHeader> = React.memo(({ chat }) => {
-  function extractUserCount(): string {
-    if (chat.type === 'dialog' && chat.opponent) {
-      return 'личные сообщения';
-    }
-    return `${chat.userCount} участник(ов)`;
-  }
+export const ChatHeader: React.FC<ChatHeader> = React.memo(
+  ({ chat, leaveChat = (): void => undefined }) => {
+    const [isMoreIconHover, setIsMoreIconHover] = useState<boolean>(false);
 
-  function extractChatName(): string {
-    if (chat.type === 'dialog' && chat.opponent) {
-      return chat.opponent.login;
+    const chatName = chat.name || '';
+
+    function extractUserCount(): string {
+      if (chat.type === 'dialog' && chat.opponent) {
+        return 'личные сообщения';
+      }
+      return `${chat.userCount} участник(ов)`;
     }
 
-    if (chat.name.length > 40) {
-      return `${chat.name.slice(0, 40)}...`;
-    }
-    return chat.name;
-  }
+    function extractChatName(): string {
+      if (chat.type === 'dialog' && chat.opponent) {
+        return chat.opponent.login;
+      }
 
-  return (
-    <header className={styles.headerContent}>
-      <section className={styles.chatInfoSection}>
-        <Avatar
-          chatType={chat.type}
-          url={chat.avatar}
-          width={40}
-          style={{ marginRight: '16px' }}
-        />
-        <div className={styles.infoSection}>
-          <span className={styles.name}>{extractChatName()}</span>
-          <span className={styles.userCount}>{extractUserCount()}</span>
+      if (chatName.length > 40) {
+        return `${chatName.slice(0, 40)}...`;
+      }
+      return chatName;
+    }
+
+    function renderOptions(): React.ReactNode {
+      if (!isMoreIconHover) return null;
+
+      return (
+        <div className={styles.options} onClick={(): void => leaveChat()}>
+          Leave the chat
         </div>
-        <span className={styles.moreIcon}>
-          <MoreIcon />
-        </span>
-      </section>
-    </header>
-  );
-});
+      );
+    }
+
+    const MemoizedOptions = useMemo(renderOptions, [
+      isMoreIconHover,
+      leaveChat,
+    ]);
+
+    return (
+      <header className={styles.headerContent}>
+        <section className={styles.chatInfoSection}>
+          <Avatar
+            chatType={chat.type}
+            url={chat.avatar}
+            width={40}
+            style={{ marginRight: '16px' }}
+          />
+          <div className={styles.infoSection}>
+            <span className={styles.name}>{extractChatName()}</span>
+            <span className={styles.userCount}>{extractUserCount()}</span>
+          </div>
+          <span
+            className={styles.moreIcon}
+            onMouseEnter={(): void => setIsMoreIconHover(true)}
+            onMouseLeave={(): void => setIsMoreIconHover(false)}
+          >
+            <MoreIcon />
+            {MemoizedOptions}
+          </span>
+        </section>
+      </header>
+    );
+  },
+);
