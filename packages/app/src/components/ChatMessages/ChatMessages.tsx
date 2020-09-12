8b525702ff45af7904ee27daa03f45ffda254b40
reginPekin <reginbegin@gmail.com>
2020-09-12 11:07:41 +0300

    Add BioPopup

--- packages/app/src/components/ChatMessages/ChatMessages.tsx
+++ packages/app/src/components/ChatMessages/ChatMessages.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
+import { BioContext } from '../App';
 
 import { Message } from '@chickenhan/components/src/Message';
 
 import { Chat, User } from '@chickenhan/sdk/lib/types';
 
 import { useStore } from '../../store';
+
+import styles from './ChatMessages.module.css';
 
 interface ChatMessagesProps {
   chat: Chat;
@@ -18,12 +21,19 @@ export const ChatMessages: React.FC<ChatMessagesProps> = ({
   getMessagesRef,
 }) => {
   const store = useStore();
-  const messagesApi = store.messages.get(chat.id);
+  const messagesApi = store.messages.get(chat.chatId);
   const chatMessages = store.messages.useSelector(
-    messages => messages[chat.id],
+    messages => messages[chat.chatId],
   );
 
+  const { changeBioId } = useContext(BioContext);
+
   const messagesRef = getMessagesRef();
+
+  function openBioPopup(id: number): void {
+    changeBioId(id);
+    store.local.update({ isBioPopupOpen: true });
+  }
 
   useEffect(() => {
     (async function fetchMessages(): Promise<any> {
@@ -34,15 +44,21 @@ export const ChatMessages: React.FC<ChatMessagesProps> = ({
         }
       }, 50);
     })();
-  }, [chat.id]);
+  }, [chat.chatId]);
 
   if (!chatMessages?.messages || chatMessages.messages.length === 0) {
-    return <div>Напиши первое сообщение</div>;
+    return (
+      <div className={styles.emptyMessagesSection}>Write first message</div>
+    );
   }
   return (
     <>
       {chatMessages.messages.map(message => (
-        <Message message={message} key={message.messageId} />
+        <Message
+          message={message}
+          key={message.messageId}
+          openBioPopup={(id: number): void => openBioPopup(id)}
+        />
       ))}
       {(chatMessages.pendingMessages || []).map(message => (
         <Message
