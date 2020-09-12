91db65ba89a711520c524bcd55b6bf7f5531d13c
reginPekin <reginbegin@gmail.com>
2020-09-12 11:12:42 +0300

    Make select component more type flexible

--- packages/components/src/Select/Select.tsx
+++ packages/components/src/Select/Select.tsx
@@ -4,65 +4,67 @@ import styles from './Select.module.css';
 
 import { useOnClickOutside } from '../utils/hooks';
 
-interface Option {
+interface Option<T> {
   label: string;
-  value: string;
+  value: T;
   description?: string;
   selected?: boolean;
 }
 
-interface SelectProps {
-  options: Option[];
+interface SelectProps<T> {
+  options: Option<T>[];
   title: string;
 
-  setSelectedOption?: (value: string) => void;
+  setSelectedOption?: (option: Option<T>) => void;
 }
 
-export const Select: React.FC<SelectProps> = React.memo(
-  ({ title, options, setSelectedOption = (): void => undefined }) => {
-    const defaultValue = options.filter(option => option.selected);
+export const Select = <T,>({
+  title,
+  options,
+  setSelectedOption = (): void => undefined,
+}: SelectProps<T>): JSX.Element => {
+  const defaultValue = options.filter(option => option.selected);
 
-    const selectRef = useRef<HTMLDivElement | null>(null);
+  const selectRef = useRef<HTMLDivElement | null>(null);
 
-    const [chosenOption, setChosenOption] = useState<Option>(defaultValue[0]);
-    const [isOpened, setIsOpened] = useState<boolean>(false);
+  const [chosenOption, setChosenOption] = useState<Option<T>>(defaultValue[0]);
+  const [isOpened, setIsOpened] = useState<boolean>(false);
 
-    useOnClickOutside(selectRef, () => setIsOpened(false));
+  useOnClickOutside(selectRef, () => setIsOpened(false));
 
-    useEffect(() => {
-      setChosenOption(defaultValue[0]);
-      setSelectedOption(defaultValue[0].value);
-    }, []);
+  useEffect(() => {
+    setChosenOption(defaultValue[0]);
+    setSelectedOption(defaultValue[0]);
+  }, []);
 
-    return (
-      <section className={styles.select} ref={selectRef}>
-        <div
-          className={styles.selectSection}
-          onClick={(): void => setIsOpened(!isOpened)}
-        >
-          <span className={styles.selecTitle}>{title}</span>
-          <span>{chosenOption.label}</span>
-        </div>
-        <section
-          className={styles.dropDownSelect}
-          style={{ display: isOpened ? 'flex' : 'none' }}
-        >
-          {options.map((option, key) => (
-            <div
-              key={key}
-              className={styles.option}
-              onClick={(): void => {
-                setIsOpened(false);
-                setChosenOption(option);
-                setSelectedOption(option.value);
-              }}
-            >
-              <span className={styles.label}>{option.label}</span>
-              <span className={styles.description}>{option.description}</span>
-            </div>
-          ))}
-        </section>
+  return (
+    <section className={styles.select} ref={selectRef}>
+      <div
+        className={styles.selectSection}
+        onClick={(): void => setIsOpened(!isOpened)}
+      >
+        <span className={styles.selecTitle}>{title}</span>
+        <span>{chosenOption.label}</span>
+      </div>
+      <section
+        className={styles.dropDownSelect}
+        style={{ display: isOpened ? 'flex' : 'none' }}
+      >
+        {options.map((option, key) => (
+          <div
+            key={key}
+            className={styles.option}
+            onClick={(): void => {
+              setIsOpened(false);
+              setChosenOption(option);
+              setSelectedOption(option);
+            }}
+          >
+            <span className={styles.label}>{option.label}</span>
+            <span className={styles.description}>{option.description}</span>
+          </div>
+        ))}
       </section>
-    );
-  },
-);
+    </section>
+  );
+};
